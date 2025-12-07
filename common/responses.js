179fac50bc6857ac0734750d18432882d2a1bd88
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-12-07 23:04:29 +0530

    dev:fix: aunthentication middleware updated with auth service

---
+++ common/responses.js
@@ -0,0 +1,9 @@
+module.exports = Object.freeze({
+  PAYLOAD: {
+    // /api/v1/auth
+    AUTH: {
+      // post: /
+      LOGGEDOUT: 'Logged out successfully!',
+    },
+  },
+});
---
+++ services/auth.service.js
@@ -0,0 +1,35 @@
+const authRepo = require('../repos/v1/auth.repo');
+const { STATUS_CODE } = require('../constants/app.constants');
+const { PAYLOAD } = require('../common/responses');
+
+const authService = {
+  handleLogout: async (data) => {
+    const { token, user } = data;
+
+    // insert token
+    const tokenDetails = {
+      token: token,
+      userId: user.id,
+    };
+    await authRepo.create(tokenDetails);
+
+    return {
+      success: true,
+      status: STATUS_CODE.CREATED,
+      data: {
+        message: PAYLOAD.AUTH.LOGGEDOUT,
+      },
+    };
+  },
+
+  validateToken: async (token) => {
+    var blacklistedTokens = await authRepo.getAll();
+
+    // check if token is blacklisted
+    blacklistedTokens = blacklistedTokens.filter((t) => t.token === token);
+
+    return blacklistedTokens.length === 0;
+  },
+};
+
+module.exports = authService;
